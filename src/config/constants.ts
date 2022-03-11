bf9ed8f99bfdd21f5dc0e0e5bb6e9d6238a2084f
Dorfman <98485094+Dorfmans@users.noreply.github.com>
2022-03-11 20:40:02 -0300

    Add files via upload

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://dorf:ETawanOpbec01Jew@UrlShortener.yptlq.mongodb.net/UrlShortener?retryWrites=true&w=majority',
+}
